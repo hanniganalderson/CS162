be85c947cefd3bb11605fe26a403c82e037531f7
hanniganalderson <hannigan.a0@gmail.com>
2025-04-10 10:41:10 -0700

    Python Array Assignment

---
+++ PythonArray.py
@@ -0,0 +1,7 @@
+import numpy as np
+#Creaing 5x5 array
+array = np.random.randint(0,50,size=(5,5), dtype=int)
+#Printing entire array
+print(array)
+#Printing number from 2nd row 3rd column
+print(array[1, 2])
